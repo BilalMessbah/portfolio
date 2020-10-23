f7080434cc8462179509fee45c36d04c183db300
Bilal Messbah <bilal.messbah@epitech.eu>
2020-10-23 07:32:23 +0200

    browserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
+import BrowserRouter from 'react-router-dom/BrowserRouter';
+import "bootstrap/dist/css/bootstrap.min.css";
 import 'antd/dist/antd.css';
 
 import Index from "./components/index";
@@ -9,12 +10,14 @@ import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <Router>
       {/* <Navbar/> */}
     <div className="App">
-      <Route path="/" exact component={Index}/>
+      <Route exact path="/portfolio/" exact component={Index}/>
     </div>
     </Router>
+   </BrowserRouter>
   );
 }
 
