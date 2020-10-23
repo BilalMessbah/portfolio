36367b6cd8ce7d9da499ffee84bc786216bcb987
Bilal Messbah <bilal.messbah@epitech.eu>
2020-10-23 06:41:45 +0200

    Updates

---
+++ precache-manifest.b453a1ec1ae358efa5ce8824424f8c14.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8ca237704f43fc1fea54ba9fb7ee4e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a1fd183f8abc1612490e",
+    "url": "/portfolio/static/css/2.3fedcc67.chunk.css"
+  },
+  {
+    "revision": "62f644e3831923b4e586",
+    "url": "/portfolio/static/css/main.b941d44f.chunk.css"
+  },
+  {
+    "revision": "a1fd183f8abc1612490e",
+    "url": "/portfolio/static/js/2.14891eca.chunk.js"
+  },
+  {
+    "revision": "1659e0386cd18825af2c5acf4990a0b1",
+    "url": "/portfolio/static/js/2.14891eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f644e3831923b4e586",
+    "url": "/portfolio/static/js/main.77b2db1d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b16240de93a953d893bbe1e56b51f70d",
+    "url": "/portfolio/static/media/Capture.b16240de.PNG"
+  },
+  {
+    "revision": "2bd70232965959a6bf34bc5be68f6b99",
+    "url": "/portfolio/static/media/Capture2.2bd70232.PNG"
+  },
+  {
+    "revision": "9309f1087d87f376a1e9f361679bcc61",
+    "url": "/portfolio/static/media/Capture3.9309f108.PNG"
+  },
+  {
+    "revision": "6b7e5aadbae7185e0431d3158ab7efd6",
+    "url": "/portfolio/static/media/Capture4.6b7e5aad.PNG"
+  },
+  {
+    "revision": "a05077b052065fbd4be92a7b8a443bfe",
+    "url": "/portfolio/static/media/Capture5.a05077b0.PNG"
+  },
+  {
+    "revision": "9eae18c9c2066815aefa0321fdea9988",
+    "url": "/portfolio/static/media/Capture6.9eae18c9.PNG"
+  },
+  {
+    "revision": "0df0e2d02cf42632ba506c81a83caacc",
+    "url": "/portfolio/static/media/E_STUDIO1.0df0e2d0.png"
+  },
+  {
+    "revision": "bd23d4ee0f68c7ee520be0e8e8fdf646",
+    "url": "/portfolio/static/media/accueil.bd23d4ee.PNG"
+  },
+  {
+    "revision": "c8fd98e32bad38d3e5afc5f53a6d0ec1",
+    "url": "/portfolio/static/media/blanc.c8fd98e3.png"
+  },
+  {
+    "revision": "f968361e2bb5b9cbadbfe0141db4d68c",
+    "url": "/portfolio/static/media/inspiration.f968361e.jpg"
+  },
+  {
+    "revision": "dbde664661c00f4c9f8d234eaa171e05",
+    "url": "/portfolio/static/media/login.dbde6646.PNG"
+  },
+  {
+    "revision": "7690d0c579329cb7a5d2c919e040eda9",
+    "url": "/portfolio/static/media/logo.7690d0c5.png"
+  },
+  {
+    "revision": "db77428dfc0df8c53910b3754d760700",
+    "url": "/portfolio/static/media/lorem.db77428d.PNG"
+  },
+  {
+    "revision": "27145eb7614f994cd11b3df9506d069e",
+    "url": "/portfolio/static/media/mysql.27145eb7.png"
+  },
+  {
+    "revision": "cdb35fa7f6e8a822efdd4a3adac36178",
+    "url": "/portfolio/static/media/profil.cdb35fa7.webp"
+  },
+  {
+    "revision": "cfbf83411e4fc84baa1bcc4842621592",
+    "url": "/portfolio/static/media/register.cfbf8341.PNG"
+  }
+]);
